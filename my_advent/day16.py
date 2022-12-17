97ef019b01268363c257055e11cbc697563c5bb2
Verena Lanzinger <verena.lanzinger@cloudflight.io>
2022-12-17 12:32:01 +0100

    day 16 first things

--- my_advent/day16.py
+++ my_advent/day16.py
@@ -4,10 +4,47 @@ from my_advent import get_todays_puzzle, MyPuzzle
 
 
 TIME_AVAILABLE = 30  # minutes = steps
+    
 
+class Valve:
+    def __init__(self, name: str, flow_rate: int, neighbours: list[str]):
+        self.name = name
+        self.flow_rate = flow_rate
+        self.neighbours = neighbours
+        self.open = False
+        # self.neighbours_weight
+        
+    def flow(self) -> int:
+        if open:
+            return self.flow_rate
+        return 0
+ 
+ 
+def parse_valves(inputs: list[str]) -> dict[Valve]:
+    valves = {}
+    for line in inputs:
+        name, attributes = line.replace("Valve ", "").split(" has flow rate=")
+        flow_rate, neighbours = attributes.split("; tunnels lead to valves ")
+        valves[name] = Valve(name, int(flow_rate), neighbours.split(", "))
+    return valves
+    
 
 def max_pressure_release(inputs: list[str]) -> int:
-    return 0
+    valves = parse_valves(inputs)
+    pressure_released = open_pressure = 0
+    position = "AA"
+    # currently all are closed
+    valves_to_open = [v.name for v in valves.items() if v.flow_rate > 0]
+    for i in range(TIME_AVAILABLE):
+        pressure_released += open_pressure
+        # find how to calculate which decision to take
+        # something with flow_rate / (distance + 1) ?
+        possible_routes = valves[position].neighbours
+        # if step towards a valve
+        # -> position = stepped towards
+        # elif open valve
+        # -> open_pressure += valves[position].flow_rate
+    return pressure_released
 
 
 def b(inputs: list[str]) -> int:
